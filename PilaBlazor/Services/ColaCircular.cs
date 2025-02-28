af80517a972463434b80986589af26b4b8bb00de
luis mejia <luis03mejia03@gmail.com>
2025-02-28 02:04:32 -0600

    primer commit

---
+++ PilaBlazor/Services/ColaCircular.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+
+namespace PilaBlazor.Services
+{
+    public class ColaCircular
+    {
+        public int max;
+        public string[] cola { get; set; }
+        public int frente;
+        public int final;
+        private int contador;
+
+        public ColaCircular(int maxx)
+        {
+            max = maxx;
+            cola = new string[max];
+            frente = 0;
+            final = 0;
+            contador = 0;
+        }
+
+        public bool ColaVacia()
+        {
+            return contador == 0;
+        }
+
+        public bool ColaLlena()
+        {
+            return contador == max;
+        }
+
+        public void Insertar(string dato)
+        {
+            if (ColaLlena())
+            {
+                Console.WriteLine("La cola está llena.");
+                return;
+            }
+
+            cola[final] = dato;
+            final = (final + 1) % max;
+            contador++;
+        }
+
+        public void Eliminar()
+        {
+            if (ColaVacia())
+            {
+                Console.WriteLine("La cola está vacía.");
+                final = 0;
+                frente = 0;
+                return;
+            }
+            
+            
+
+            cola[frente] = null;
+            frente = (frente + 1) % max;
+            contador--;
+        }
+
+    }
+}
+
+
